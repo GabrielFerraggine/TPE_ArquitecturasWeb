d4cb5b965cf34ad9f51c6ac7b6e16906f64158f2
IsPe123 <Peterislas@outlook.com>
2025-11-10 11:38:41 -0300

    MicroServicio Monopatin: Ahora si, bien configurado el arranque :D

--- ms-monopatin/src/main/java/org/example/Main.java
+++ ms-monopatin/src/main/java/org/example/Main.java
@@ -1,18 +1,30 @@
 package org.example;
 
+import Utils.CargarDatos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.example", "Utils", "repository", "entity", "controller", "service"})
 @EnableJpaRepositories(basePackages = "repository")
-@EntityScan(basePackages = "entity")  // ← ESTA LÍNEA ES CLAVE
+@EntityScan(basePackages = "entity")
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
+
+        try {
+            CargarDatos cargaDeDatos = context.getBean(CargarDatos.class);
+            cargaDeDatos.cargarDatosCSV();
+
+        }   catch (Exception e) {
+            System.err.println("Error inesperado al cargar los datos: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
+
 }
